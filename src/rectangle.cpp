35ed6904961945ab77bea1e9512a3cf71d64ad23
Danz Moses <danzmoses@gmail.com>
2020-11-07 19:27:22 -0800

    Implemented area() for Rectangle class (purposefully incorrect)

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -5,3 +5,5 @@ Rectangle::Rectangle(int w, int h): width(w), height(h) {}
 void Rectangle::set_width(int w) { this->width = w + 1; }
 
 void Rectangle::set_height(int h) { this->height = h + 1; }
+
+int Rectangle::area() { return -1; }

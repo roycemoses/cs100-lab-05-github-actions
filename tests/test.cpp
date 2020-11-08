3364ed7c0d0bbfd5d49f56a8042a61b4780b6a99
Danz Moses <danzmoses@gmail.com>
2020-11-07 20:43:47 -0800

    Added ConstructorNegativeWidthAndHeight test

--- tests/test.cpp
+++ tests/test.cpp
@@ -5,14 +5,28 @@
 
 TEST(ConstructorTests, DefaultConstructor)
 {
-    Rectangle rect;
-    EXPECT_EQ(rect.area(), 0);
-    EXPECT_EQ(rect.perimeter(), 0);
+    Rectangle* rect = new Rectangle();
+    EXPECT_EQ(rect->area(), 0);
+    EXPECT_EQ(rect->perimeter(), 0);
 }
 
 TEST(ConstructorTests, ConstructorPositiveWidthAndHeight)
 {
-    Rectangle rect(2, 2);
-    EXPECT_EQ(rect.area(), 4);
-    EXPECT_EQ(rect.perimeter(), 4);
+    Rectangle* rect = new Rectangle(2, 2);
+    EXPECT_EQ(rect->area(), 4);
+    EXPECT_EQ(rect->perimeter(), 4);
+}
+
+TEST(ConstructorTests, ConstructorNegativeWidthAndHeight)
+{
+    EXPECT_THROW({
+        try {
+            Rectangle rect(-2, -2);
+        }
+        catch (std::invalid_argument& ia)
+        {
+            EXPECT_EQ("-2", ia.what());
+            throw;
+        }
+    }, std::invalid_argument);
 }

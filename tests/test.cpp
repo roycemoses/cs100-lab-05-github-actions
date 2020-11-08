a1efa75b4ef3fce54df324f595302910d4ffba69
Danz Moses <danzmoses@gmail.com>
2020-11-07 20:47:39 -0800

    Added ConstructorRationalWidthAndHeight test

--- tests/test.cpp
+++ tests/test.cpp
@@ -14,7 +14,7 @@ TEST(ConstructorTests, ConstructorPositiveWidthAndHeight)
 {
     Rectangle* rect = new Rectangle(2, 2);
     EXPECT_EQ(rect->area(), 4);
-    EXPECT_EQ(rect->perimeter(), 4);
+    EXPECT_EQ(rect->perimeter(), 8);
 }
 
 TEST(ConstructorTests, ConstructorNegativeWidthAndHeight)
@@ -36,4 +36,11 @@ TEST(ConstructorTests, ConstructorZeroWidthAndHeight)
     Rectangle* rect = new Rectangle(0, 0);
     EXPECT_EQ(rect->area(), 0);
     EXPECT_EQ(rect->perimeter(), 0);
+}
+
+TEST(ConstructorTests, ConstructorRationalWidthAndHeight)
+{
+    Rectangle* rect = new Rectangle(2.25, 2.25);
+    EXPECT_EQ(rect->area(), 4);
+    EXPECT_EQ(rect->perimeter(), 8);
 }

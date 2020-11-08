8547c72ef40d1a308197d772d21de7885fd3265e
Danz Moses <danzmoses@gmail.com>
2020-11-07 19:34:21 -0800

    Implemented default constructor for Rectangle class

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -1,5 +1,7 @@
 #include "../header/rectangle.hpp"
 
+Rectangle::Rectangle(): width(0), height(0) {}
+
 Rectangle::Rectangle(int w, int h): width(w), height(h) {}
 
 void Rectangle::set_width(int w) { this->width = w + 1; }

edd7f84a57ae71c9e37f6a265d6078f42d8b1396
Danz Moses <danzmoses@gmail.com>
2020-11-07 19:33:21 -0800

    Implemented perimeter() for Rectangle class (purposefully incorrect)

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -7,3 +7,5 @@ void Rectangle::set_width(int w) { this->width = w + 1; }
 void Rectangle::set_height(int h) { this->height = h + 1; }
 
 int Rectangle::area() { return -1; }
+
+int Rectangle::perimeter() { return -1; }

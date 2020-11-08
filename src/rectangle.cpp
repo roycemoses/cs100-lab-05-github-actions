cd3245eb0aac802e66fa06b8cec60a789e160650
roycemoses <royce.kmoses@gmail.com>
2020-11-07 19:23:12 -0800

    Updated Rectangle constructor to throw invalid arguments

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -1,9 +1,21 @@
 #include "../header/rectangle.hpp"
+#include <stdexcept>
 
 Rectangle::Rectangle(int w, int h) :
     width(w),
     height(h)
-{}
+{ 
+    if (w < 0)
+    { 
+        std::invalid_argument ia = std::invalid_argument(std::to_string(w));
+        throw ia;
+    }
+    if (h < 0) 
+    {
+        std::invalid_argument ia = std::invalid_argument(std::to_string(h));
+        throw ia;
+    }
+}
 
 void Rectangle::set_width(int w) { this->width = w; }
 void Rectangle::set_height(int h) { this->height = h; }

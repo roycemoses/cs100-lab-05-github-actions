4909c8b8b6327c3ee4ac42ce6bcd63ed53229083
roycemoses <royce.kmoses@gmail.com>
2020-11-07 20:34:49 -0800

    Added PerimeterReturnPositive test

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,3 +1,10 @@
 #include "rectangle.hpp"
 #include <stdexcept>
 #include "gtest/gtest.h"
+
+TEST(PerimeterTests, PerimeterReturnPositive)
+{
+    Rectangle* rect = new Rectangle(2, 2);
+
+    EXPECT_EQ(rect->perimeter(), 8);
+}

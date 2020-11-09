c9d3fa37ab0f68b7667539c150316b98b93415af
danzmoses <danzmoses@gmail.com>
2020-11-08 22:05:14 -0800

    Implemented get_width() and get_height() for Rectangle class

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -8,6 +8,10 @@ void Rectangle::set_width(int w) { this->width = w + 1; }
 
 void Rectangle::set_height(int h) { this->height = h + 1; }
 
+int Rectangle::get_width() { return this->width; }
+
+int Rectangle::get_height() { return this->height; }
+
 int Rectangle::area() { return -1; }
 
 int Rectangle::perimeter() { return -1; }
